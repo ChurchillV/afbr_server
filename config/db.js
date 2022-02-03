63519344ecac066d404df2975890837944db4685
YawOtuo <yotuo2003@gmail.com>
2022-02-03 22:19:55 +0000

    changing database users and stuff

--- config/db.js
+++ config/db.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection({
     host: "z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "ks8pa75ol3h0skzv",
     password:"el5w7f3rjcbisebg",
-    database: "el5w7f3rjcbisebg"
+    database: "jwjgkj993t2yf6sa"
 })
 
 

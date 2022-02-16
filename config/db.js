bf01fd05c5985620724c7a0e1e8268c5c2387871
YawOtuo <yotuo2003@gmail.com>
2022-02-16 17:30:34 +0000

    addtions

--- config/db.js
+++ config/db.js
@@ -10,14 +10,14 @@
 //   };
 const mysql = require('mysql')
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'afbr'
-    // host: "z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    // user: "ks8pa75ol3h0skzv",
-    // password:"el5w7f3rjcbisebg",
-    // database: "jwjgkj993t2yf6sa"
+    // host: '127.0.0.1',
+    // user: 'root',
+    // password: '',
+    // database: 'afbr'
+    host: "z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "ks8pa75ol3h0skzv",
+    password:"el5w7f3rjcbisebg",
+    database: "jwjgkj993t2yf6sa"
 })
 
 

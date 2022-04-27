fddb1cfb1db7ab94fad14ad5db294b68f8692807
YawOtuo <yotuo2003@gmail.com>
2022-04-27 15:12:46 -0700

    redicrect_url

--- controllers/dpo_controllers.js
+++ controllers/dpo_controllers.js
@@ -23,7 +23,7 @@ exports.postTransact = (req, res) => {
         "<PaymentCurrency>USD</PaymentCurrency>" +
         "<CompanyRef>49FKEOA</CompanyRef>" +
         `<RedirectURL>${RedirectURL}</RedirectURL>` +
-        "<BackURL>http://afbr-80930.web.app/dog_registrations_success</BackURL>" +
+        `<BackURL>${RedirectURl}</BackURL>` +
         "<CompanyRefUnique>0</CompanyRefUnique>" +
         "<PTL>5</PTL>" +
         "</Transaction>" +

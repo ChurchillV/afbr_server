b9eb88860c7865451687c9fb3bd641dc0525299d
YawOtuo <yotuo2003@gmail.com>
2022-05-01 18:55:57 -0700

    redirect url

--- controllers/dpo_controllers.js
+++ controllers/dpo_controllers.js
@@ -7,29 +7,30 @@ exports.postTransact = (req, res) => {
     let transaction_name = req.body.transaction_name
     let transaction_cost = req.body.transaction_cost
     console.log(transaction_cost)
+    let RedirectURL = ''
 
-    // transaction_name = 'Litter Registrations ' ? 
-    //  RedirectURL = 'http://afbr-80930.web.app/dog_registrations_success'
-    //  :      RedirectURL = 'http://afbr-80930.web.app/litter_registrations_success'
+    transaction_name = 'Litter Registrations ' ? 
+    RedirectURL = 'http://afbr-80930.web.app/litter_registrations_success'
+:     RedirectURL = 'http://afbr-80930.web.app/dog_registrations_success'
 
-
+    //  http://afbr-80930.web.app/dog_registrations_success
     var dpo_data =
         "<?xml version='1.0' encoding='utf-8'?>" +
         "<API3G>" +
-        "<CompanyToken>B069C2B2-B27D-42DE-B913-0B539235D0F6</CompanyToken>" +
+        "<CompanyToken>8D3DA73D-9D7F-4E09-96D4-3D44E7A83EA3</CompanyToken>" +
         "<Request>createToken</Request>" +
         "<Transaction>" +
         `<PaymentAmount>${transaction_cost}</PaymentAmount>` +
         "<PaymentCurrency>USD</PaymentCurrency>" +
         "<CompanyRef>49FKEOA</CompanyRef>" +
-        "<RedirectURL>http://afbr-80930.web.app/dog_registrations_success</RedirectURL>" +
+        `<RedirectURL>${RedirectURL}</RedirectURL>` +
         "<BackURL>http://afbr-80930.web.app/dog_registrations_success</BackURL>" +
         "<CompanyRefUnique>0</CompanyRefUnique>" +
         "<PTL>5</PTL>" +
         "</Transaction>" +
         "<Services>" +
         "  <Service>" +
-        "  <ServiceType>51851</ServiceType>" +
+        "  <ServiceType>3854</ServiceType>" +
         `<ServiceDescription>${transaction_name}</ServiceDescription>}` +
         `<ServiceDate>${today}</ServiceDate>` +
         " </Service>" +

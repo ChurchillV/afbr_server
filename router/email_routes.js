ac91fc5e60851c808b968c02919a6d5f7999544e
YawOtuo <yotuo2003@gmail.com>
2022-05-20 14:03:22 +0000

    refactoring emails

--- router/email_routes.js
+++ router/email_routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { postSendRegisterEmail , postSendRegisterDogEmail, postLitter, postPedigree, contactUs} = require('../controllers/email_controllers')
+const { postSendRegisterEmail , postSendRegisterDogEmail, postLitter, postPedigree} = require('../controllers/email_controllers')
 
 router.post('/email_register', postSendRegisterEmail)
 
@@ -13,7 +13,6 @@ router.post('/litter', postLitter)
 router.post('/pedigree', postPedigree)
 
 
-router.post('/contact', contactUs)
 
 
 

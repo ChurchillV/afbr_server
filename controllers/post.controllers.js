2c2ed05478a9fb5bcb6a86d4efe6585433cfd506
ChurchillV <104873546+ChurchillV@users.noreply.github.com>
2022-11-26 13:25:24 +0000

    Including CRUD operations

--- controllers/post.controllers.js
+++ controllers/post.controllers.js
@@ -3,10 +3,15 @@ const db = require('../config/db')
 const axios = require('axios')
 const url2 = require('../weburl')
 const url = url2['url']
+const express = require('express')
+const app = express()
+const router = express.Router()
+const multer = require('multer')
 
 const  {dog} = require('../models')
 const  {users}  = require('../models')
 const {posts} = require('../models')
+const { post, data } = require('jquery')
 
 exports.getAllPosts = (req, res) => {
         posts.findAll()
@@ -18,7 +23,83 @@ exports.getAllPosts = (req, res) => {
             {
                 console.log(err)
             })
-    
-    
-    
+};
+
+//Get one post
+exports.getOnePost = (req,res) => {
+    const id = req.params.id
+        posts.findByPk(id)
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `Cannot find post with the id ${id}`
+                })
+            }
+        })
+            .catch(err => {
+                res.send({
+                    message: `Error finding the post with id ${id}`
+                })
+            })
 }
+
+//Create a new post
+exports.createPost = (req,res) => {
+
+        const newPost = {
+            content: req.body.content,
+            timestamp: new Date(),
+            user: req.body.user,
+            likes: req.body.likes
+        };
+
+        posts.create(newPost)
+            .then(data => {
+                res.send("Post created successfully");
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+
+//Delete a post
+exports.deletePost = (req, res) => {
+    const id = req.params.id;
+
+    posts.destroy({
+        where: {id : id}
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Post was deleted successfully"
+                });
+            }
+        })
+        .catch(err => console.log(err))
+}
+
+
+//Update a post 
+exports.updatePost = (req,res) => {
+    const id = req.params.id;
+
+    posts.update(req.body, {
+        where: {id: id}
+    })
+        .then(num => {
+            if (num => 1) {
+                res.send({
+                    message: "Post was updated successfully"
+                });
+            } else{
+                res.send({
+                    message: `Cannot update post with id ${id}. Try again`
+                });
+            }
+        })
+        .catch(err => console.log(err))
+}
+

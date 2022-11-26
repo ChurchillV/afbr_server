c111a7934c0d194bdf166daa6512d3d00c343d6a
YawOtuo <yotuo2003@gmail.com>
2022-11-26 15:30:56 +0000

    adding comments

--- router/post_routes.js
+++ router/post_routes.js
@@ -5,12 +5,8 @@ const { getAllPosts, createPost, deletePost, updatePost, getOnePost } = require(
 
 //Get all Posts
 router.get('/', getAllPosts)
-
-
 //Get one post
 router.get('/:id', getOnePost)
-
-
 //Create a new post
 router.post('/', createPost)
 
@@ -18,12 +14,14 @@ router.post('/', createPost)
 //Delete a post
 router.delete('/:id', deletePost)
 
-
-
 //Update a post
 router.put('/:id', updatePost)
 
+//please add an endpoint to add a like to a post
 
+// please implement a similar CRUD endpoint for comments too
+
+// all the best bro... you got this :)
 
 
 
